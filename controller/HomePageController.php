f2f3f4207eaac570a7a585b5611baf7d24c0e8d0
Stavros Skourtis <stavros.skourtis@gmail.com>
2015-03-12 21:32:16 +0200

    v0.1 Created basic files and folders

---
+++ controller/HomePageController.php
@@ -0,0 +1,13 @@
+<?php
+
+    class HomePageController extends Controller{
+
+        public function __construct(){
+            $this->setTitle("Home Page");
+        }
+
+        public function handle(){
+
+        }
+
+    }
