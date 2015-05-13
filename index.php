be24d4cfdc45ece830d682f6ef4f2fa0d30f88eb
Stavros Skourtis <stavros.skourtis@gmail.com>
2015-05-13 22:18:27 +0300

    v0.44 session timeout

--- index.php
+++ index.php
@@ -5,8 +5,14 @@
     /*
         Starting point of this application
     */
+    session_start();    
     include 'utils/Controller.php';
-    
+    if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+        // last request was more than 30 minutes ago
+        session_unset();     // unset $_SESSION variable for the run-time 
+        session_destroy();   // destroy session data in storage
+    }
+    $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
     
 
     /*
--- view/header.php
+++ view/header.php
@@ -1,5 +1,3 @@
-<?php session_start() ?>
-
 <div id="Header">
     <div id="HeaderContent">
         <div id="HeaderLeft">
